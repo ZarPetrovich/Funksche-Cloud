fb14eab402aad3cfe09ddcc734e8ad94e75129d0
Zar_Petr <116543485+ZarPetrovich@users.noreply.github.com>
2023-04-18 15:45:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,34 @@
-#include <DMXSerial.h>
-#include <FastLED.h>
+#include <arduino.h>
+#include <esp_dmx.h>
 
-const int LED_PIN = 14; // Replace with your LED pin
-#define DMXChannels = 8
-const int DMX_CHANNEL_BRIGHTNESS = 1; // Replace with your DMX channel for brightness
-const int DMX_CHANNEL_COLOR1_R = 2; // Replace with your DMX channel for color1 red
-const int DMX_CHANNEL_COLOR1_G = 3; // Replace with your DMX channel for color1 green
-const int DMX_CHANNEL_COLOR1_B = 4; // Replace with your DMX channel for color1 blue
-const int NUM_LEDS = 30; // Replace with the number of LEDs in your strip
-CRGB leds[NUM_LEDS]; // Array to store LED colors
 
-unsigned long lastReadTime = 0; // Variable to store the last read time
-int dmxData[4]; // Array to store the DMX data for brightness and color1
+int RX_Pin = 16;
+dmx_port_t dmxPort = 1;
+byte dmxdata [7];
+QueueHandle_t queue;
+bool dmxIsConnected = false;
 
 void setup() {
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  DMXSerial.init(DMXReceiver);
-  DMXSerial.maxChannel(4);
-}
+  Serial.begin(115200);
+  dmx_config_t dmxConfig = DMX_DEFAULT_CONFIG;
+  dmx_param_config(dmxPort, &dmxConfig);
+  dmx_set_pin(dmxPort, DMX_PIN_NO_CHANGE, RX_Pin, DMX_PIN_NO_CHANGE);
+  int queueSize = 1;
+  int interruptPriority = 1;
+  dmx_driver_install(dmxPort, DMX_MAX_PACKET_SIZE, queueSize, &queue,
+                     interruptPriority);
 
-void loop() {
-  // Read DMX data once every second
-  if (millis() - lastReadTime >= 1000) {
-    lastReadTime = millis();
-    for (int i = 0; i < 4; i++) {
-      dmxData[i] = DMXSerial.read(i + 1);
-    }
-    
-  void printDMXChart(int* dmxData, int DMXChannels) {
-    // Assign DMX data to variables
-    // int dmxBrightness = dmxData[0];
-    // int dmxColor1R = dmxData[1];
-    // int dmxColor1G = dmxData[2];
-    // int dmxColor1B = dmxData[3];
-
-    // Use DMX data to control LED brightness and color
-    // float brightness = (float) dmxBrightness / 255;
-    // CRGB color1(dmxColor1R, dmxColor1G, dmxColor1B);
-    // fill_gradient(leds, 0, color1, NUM_LEDS, CRGB::Black, CRGB::Black, FORWARD_LIN, NOBLEND);
-    // FastLED.show();
-
-    
-  }
 }
 
 
+void loop () {
 
-void printDMXChart(int* dmxData, int numChannels) {
-  Serial.println("DMX Chart:");
-  for (int i = 0; i < numChannels; i++) {
-    Serial.print(i + 1);
-    Serial.print(": ");
-    Serial.println(dmxData[i]);
+dmx_event_t packet;
+
+if (xQueueReceive(queue, &packet, DMX_PACKET_TIMEOUT_TICK)) {
+
+  if (packet.status == DMX_OK) 
+  dmx_read_packet(dmxPort, dmxdata, packet.size);
   }
-  Serial.println();
+
 }
